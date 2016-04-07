fc51c023e7b28332f8ed0ddfb6a6c35b015421c1
Adam Breznicky <adam.breznicky@txdot.gov>
2016-04-07 11:45:51 -0500

    DUSA

--- DUSA/Python/create_fc_updates_v1.py
+++ DUSA/Python/create_fc_updates_v1.py
@@ -30,10 +30,37 @@ import arcpy, os
 publish_db = "C:\\TxDOT\\Scripts\\javascript\\RIU\\Python\\Publish.gdb"
 arcpy.env.workspace = "in_memory"
 
+
+def create_domains():
+    arcpy.CreateDomain_management(publish_db, "RDWY_UPDATE", "Inventory Update Type", "TEXT", "CODED")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_UPDATE", "Add", "Add Road")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_UPDATE", "Remove", "Remove Road")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_UPDATE", "Update", "Update Attributes/Alignment")
+
+    arcpy.CreateDomain_management(publish_db, "RDWY_DESIGN", "Road Design", "SHORT", "CODED")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_DESIGN", 1, "One Way")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_DESIGN", 2, "Two Way")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_DESIGN", 3, "Boulevard")
+
+    arcpy.CreateDomain_management(publish_db, "RDWY_SURFACE", "Road Surface Type", "SHORT", "CODED")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_SURFACE", 12, "Dirt/Natural")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_SURFACE", 13, "Gravel")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_SURFACE", 10, "Paved")
+    arcpy.AddCodedValueToDomain_management(publish_db, "RDWY_SURFACE", 1, "Concrete")
+
+
 def create_feature_class(name):
     sr = arcpy.SpatialReference("WGS 1984 Web Mercator (auxiliary sphere)")
     arcpy.CreateFeatureclass_management(publish_db, name, "POLYLINE", "", "DISABLED", "DISABLED", sr)
 
+
+def create_fields(name):
+    fc = publish_db + os.sep + name
+    if name == "LocalStreet_Updates":
+
+
+create_domains()
 create_feature_class("LocalStreets_Updates")
+create_fields("LocalStreets_Updates")
 
 print "that's all folks!!"
